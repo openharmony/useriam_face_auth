6190dfed8d79dc69d4a0cf22c9dcd1e6cbc06721
dengjiewen <dengjiewen1@huawei.com>
2023-07-01 04:33:54 +0000

    move ipc code to single .h file and add CODEOWNERS file
    Signed-off-by: dengjiewen <dengjiewen1@huawei.com>
    Change-Id: I4417f0f34059d110d579594987bdd6759b83b017

--- frameworks/ipc/inc/iface_auth_ipc_interface_code.h
+++ frameworks/ipc/inc/iface_auth_ipc_interface_code.h
@@ -13,34 +13,18 @@
  * limitations under the License.
  */
 
-#ifndef IFACE_AUTH_H
-#define IFACE_AUTH_H
-#include <list>
-#include <vector>
+#ifndef IFACE_AUTH_IPC_INTERFACE_CODE_H
+#define IFACE_AUTH_IPC_INTERFACE_CODE_H
 
-#include "iremote_broker.h"
-#include "iremote_object.h"
-#include "ibuffer_producer.h"
-
-#include "iface_auth_ipc_interface_code.h"
-#include "face_auth_defines.h"
-
+/* SAID: 942 */
 namespace OHOS {
 namespace UserIam {
 namespace FaceAuth {
-class IFaceAuth : public IRemoteBroker {
-public:
-    DECLARE_INTERFACE_DESCRIPTOR(u"ohos.faceauth.IFaceAuth");
-
-    /**
-     * @brief Set buffer producer for enroll preview.
-     *
-     * @return 0 success, others failure.
-     */
-    virtual int32_t SetBufferProducer(sptr<IBufferProducer> &producer) = 0;
+enum class IFaceAuthInterfaceCode {
+    FACE_AUTH_SET_BUFFER_PRODUCER = 1,
 };
 } // namespace FaceAuth
 } // namespace UserIam
 } // namespace OHOS
 
-#endif // IFACE_AUTH_H
+#endif // IFACE_AUTH_IPC_INTERFACE_CODE_H
