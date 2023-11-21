cc290aff1f4c0321bda365cb3ba9f3fd722e8ac0
lbl <liubaolong8@huawei.com>
2023-11-21 17:59:37 +0800

    error clean
    Signed-off-by: lbl <liubaolong8@huawei.com>

--- ui/Settings_FaceAuth/entry/src/main/ets/config/constant.ts
+++ ui/Settings_FaceAuth/entry/src/main/ets/config/constant.ts
@@ -14,33 +14,12 @@
  */
 
 export default class Constants {
-  static ohos_id_color_button_normal = '#0C182431';
-  static ohos_id_color_click_effect = '#19182431';
   static ohos_id_color_control_actived = '#007DFF';
   static ohos_id_color_control_highlight = '#FF007DFF';
-  static ohos_id_color_dialog_bg = '#FFFFFF';
   static ohos_id_color_emphasize = '#007DFF';
-  static ohos_id_color_foreground = '#182431';
   static ohos_id_color_foreground_60_transparency = '#99182431';
   static ohos_id_color_foreground_97_transparency = '#08182431';
-  static ohos_id_color_foreground_inverse = 0xffffff;
-  static ohos_id_color_list_separator = '#33182431';
-  static ohos_id_color_text_hyperlink = '#007DFF';
-  static ohos_id_color_text_primary = '#182431';
-  static ohos_id_color_text_primary_actived = '#007DFF';
-  static ohos_id_color_text_secondary = '#99182431';
-  static ohos_id_color_title_bar_sub_bg = '#F9FAFB';
-  static ohos_id_color_titlebar_text = '#E5000000';
-  static ohos_id_color_warning = '#FA2A2D';
-  static ohos_id_text_button1 = '16fp';
-  static ohos_id_text_font_family_bold = 'blod';
-  static ohos_id_text_font_family_medium = 'HwChinese-medium';
-  static ohos_id_text_font_family_regular = 'sans-serif';
   static ohos_id_text_size_body1 = '16fp';
-  static ohos_id_text_size_body2 = '14fp';
-  static ohos_id_text_size_caption1 = '10vp';
   static ohos_id_text_size_dialog_title = '20fp';
-  static ohos_id_text_size_headline7 = '24vp';
-  static ohos_id_text_size_headline8 = '20vp';
   static ohos_id_text_size_subtitle1 = '18fp';
 }
